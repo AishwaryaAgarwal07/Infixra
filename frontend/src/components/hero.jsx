44588d049af5c428c2afb587efb4412fd4ad7ff6
ARSHAD <marshadkhn89@gmail.com>
2023-09-15 12:08:47 +0530

    working on hero

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -3,20 +3,16 @@ import { heroBg, topHeroImage } from "../assets";
 
 const Hero = () => {
   return (
-    <div style={{ height: '110vh'}} className="bg-custom-name blur bg-cover bg-no-repeat bg-center bg-cover">
-      <div className="">
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
-        <h1>ARSHAD</h1>
+    <div className="mt-20 relative">
+      <div
+        style={{ height: "110vh" }}
+        className="bg-custom-name z-0 bg-cover bg-no-repeat bg-center"
+      ></div>
+      <div className="absolute mt-12 top-0 left-0 z-10">
+        <h1 className="z-20">ARSHAD</h1>
       </div>
     </div>
   );
 };
 
 export default Hero;
- 

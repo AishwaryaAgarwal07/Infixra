704f24223a74f4550d585617f08a31d5e022eddf
ARSHAD <marshadkhn89@gmail.com>
2023-09-19 18:22:29 +0530

    worked on hero

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -8,15 +8,22 @@ const Hero = () => {
       <div style={{ height: "110vh" }} className="bg-custom-name z-0 bg-cover bg-no-repeat bg-center"></div>
 
       <div className="absolute mt-12 top-0 left-0 z-10 flex flex-row">
-        <div className="flex flex-col ml-20 mt-14">
+        {/* Hero text div */}
+        <div className="flex flex-col ml-20 mt-5">
           <b><p className="text-3xl">One Route Solutions for your <b className="text-yellow-500">Home | Office</b></p></b>
           <ul className="list-disc ml-5 mt-8 text-xl space-y-4">
             <li>Architecture</li>
             <li>Renovation</li>
             <li>Interior design</li>
           </ul>
+
+          {/* Hero button div*/}
+          <div className="flex rounded text-2xl mt-44 text-black cursor-pointer">
+            <p className="p-2 rounded bg-heroButton">Book a free consultation</p>
+            <p className="p-2 rounded hover:ml-3 bg-heroButton ml-1">→</p>
+          </div>
         </div>
-        {/* <img className="h-1/5 mx-40" src={topHeroImage} alt="Left House Image" /> */}
+        <img className="h-100 ml-60" src={topHeroImage} alt="Left House Image" />
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,17 +11,20 @@ export default {
         footerColor:"#061124",
         navColor:"#0b1015",
         hover:"#1c6aab",
-        heading:"#161950"
+        heading:"#161950",
+        heroButton: "#f6ec72"
       },
       screens: {
         xs: "450px", 
       },
+      height: {
+        '100': '35rem',
+        '125': '50rem',
+      },
       backgroundImage: {
 
         "work-bg": "url('/src/assets/workbg.jpg')",
-
         'custom-name': "url('/src/assets//Tophouse.jpg')",
-
         "Quotes-Bg":"url('/src/assets//quotesbg.svg')",
       }
     },

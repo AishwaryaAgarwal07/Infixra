fc1023600e1376355456e6ba105cd09a45cd05ce
ARSHAD <marshadkhn89@gmail.com>
2023-09-28 19:59:26 +0530

    initial-commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
-import Hero from './hero';
-import Projects from './Projects';
-import Quotes from './quoets';
-import HowItWorks from './HowItWorks';
-import Why from './why';
-import { service } from '../assets'
+import React from "react";
+import Hero from "./hero";
+import Projects from "./Projects";
+import Quotes from "./quoets";
+import HowItWorks from "./HowItWorks";
+import Why from "./why";
+import { service } from "../assets";
+
+const Cards = ({ image, heading,description,button }) => {
+  return (
+    <div>
+      <h1>{service.title}</h1>
+    </div>
+  );
+};
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
+  return (
+    <>
+      <Hero />
 
-            {/* Services */}
-            <div className="relative">
-                <div className='h-96 bg-services-background z-0 bg-cover bg-no-repeat bg-center'>
-                    <div className="absolute z-10 flex flex-row text-xs mt-36">
-                        <div className='flex flex-col'>
-                        <h1 className='text-black ml-2'>Services we</h1>
-                        <h1 className='text-black ml-12'>provide</h1>
-                        </div>
-                        <img className="h-16 mt-2.5" src={service} alt="arrow-image" />
-                        
-                    </div>
-                </div>
+      {/* Services */}
+      <div className="relative">
+        <div className="h-96 bg-services-background z-0 bg-cover bg-no-repeat bg-center">
+          <div className="absolute z-10 flex flex-row text-xs mt-36">
+            <div className="flex flex-col">
+              <h1 className="text-black ml-2">Services we</h1>
+              <h1 className="text-black ml-12">provide</h1>
             </div>
+            <img className="h-16 mt-2.5" src={service} alt="arrow-image" />
+            <div>
+              {service.map((works, index) => (
+                <Cards key={service.key}/>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <Projects />
-            <Quotes />
-            <HowItWorks />
-            <Why />
-        </>
-    )
-}
+      <Projects />
+      <Quotes />
+      <HowItWorks />
+      <Why />
+    </>
+  );
+};
 
 export default Home;
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,209 +1,121 @@
 import {
-    p1,
-    p2,
-    p3,
-    p4,
-    p5,
-    p6,
-    p7,
-    p8,
-
-    office,
-    corporate,
-    resort,
-    factory,
-    retail,
-    Appartment,
-    villa,
-    GuestHouse,
-    FarmHouse,
-    house,
-  
-    residential,
-    industrial,
-    commercial
-
-} from "../assets"
-
-import blogDetails from "./featuredBlog"
-
-
-export {
-    blogDetails
-}
-
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  residential,
+  industrial,
+  commercial,
+} from "../assets";
 
 export const navLinks = [
-    {
-        link: "/",
-        title: "Home",
-    },
-    {
-        link: "/portfolio",
-        title: "Portfolio",
-    },
-    {
-        link: "/e-Monitoring",
-        title: "E-Monitoring",
-    },
-    {
-        link: "/contact",
-        title: "Contact us",
-    },
+  {
+    link: "/",
+    title: "Home",
+  },
+  {
+    link: "/services",
+    title: "Services",
+  },
+  {
+    link: "/portfolio",
+    title: "Portfolio",
+  },
+  {
+    link: "/e-Monitoring",
+    title: "E-Monitoring",
+  },
+  {
+    link: "/aboutUs",
+    title: "About us",
+  },
+  {
+    link: "/contact",
+    title: "Contact us",
+  },
 ];
 
 export const work = [
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p1,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p2,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p3,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p4,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p5,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p6,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p7,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
-    {
-        title: "PQ 21",
-        place: "Xza , Karnatak",
-        image: p8,
-        size: "2000 sft",
-        xyz: "G+1"
-    },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p1,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p2,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p3,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p4,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p5,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p6,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p7,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
+  {
+    title: "PQ 21",
+    place: "Xza , Karnatak",
+    image: p8,
+    size: "2000 sft",
+    xyz: "G+1",
+  },
 ];
 
 export const services = [
-    {
-        image: residential,
-        heading: "RESIDENTIAL CONSTRUCTION",
-        description: "Appartment/lndependent house/Villa Guest house/Farm house",
-        button: "BOOK NOW"
-    },
-    {
-        image: commercial,
-        heading: "COMMERCIAL CONSTRUCTION",
-        description: "Commercial complex/Corporate office/Showroom/Retail/Industry/Hotel/Resot",
-        button: "BOOK NOW"
-    },
-    {
-        image: industrial,
-        heading: "INDUSTRIAL CONSTRUCTION",
-        description: "Factory/lndustry/warehouse",
-        button: "BOOK NOW"
-    },
-    
-]
-
-
-export const commercialCons = [
-    {
-        title: "Office",
-        image: office
-    },
-    {
-        title: "Retail Showroom",
-        image: retail
-    },
-    {
-        title: "Corporate office",
-        image: corporate
-    },
-    {
-        title: "Hotel/resort",
-        image: resort
-    },
-    {
-        title: "Factory/industry",
-        image: factory
-    },
-
-]
-export const IndustrialCons = [
-    {
-        title: "Office",
-        image: office
-    },
-    {
-        title: "Retail Showroom",
-        image: retail
-    },
-    {
-        title: "Corporate office",
-        image: corporate
-    },
-    {
-        title: "Hotel/resort",
-        image: resort
-    },
-    {
-        title: "Factory/industry",
-        image: factory
-    },
-
-]
-
-export const residentialCons = [
-    {
-        title: "Appartment",
-        image: Appartment
-    },
-    {
-        title: "Villa",
-        image: villa
-    },
-    {
-        title: "Independent House",
-        image: house
-    },
-    {
-        title: "Farm House",
-        image: FarmHouse
-    },
-    {
-        title: "Guest House",
-        image: GuestHouse
-    },
-
-]
-
-
-
+  {
+    image: residential,
+    heading: "RESIDENTIAL CONSTRUCTION",
+    description: "Appartment/lndependent house/Villa Guest house/Farm house",
+    button: "BOOK NOW",
+  },
+  {
+    image: commercial,
+    heading: "COMMERCIAL CONSTRUCTION",
+    description:
+      "Commercial complex/Corporate office/Showroom/Retail/Industry/Hotel/Resot",
+    button: "BOOK NOW",
+  },
+  {
+    image: industrial,
+    heading: "INDUSTRIAL CONSTRUCTION",
+    description: "Factory/lndustry/warehouse",
+    button: "BOOK NOW",
+  },
+];

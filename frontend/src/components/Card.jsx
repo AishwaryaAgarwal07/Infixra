be48939134b030d9936d4674c55a2e2cc74036dd
Adityamishra9719 <adityamishra971916@gmail.com>
2023-10-04 16:16:05 +0530

    blog page done

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link} from 'react-router-dom';
+
+
+const Card = ({
+    image,
+    date,
+    title,
+    category,
+    description,
+    id,
+}) => {
+    return (
+        <div className="p-10">
+            <div className=" w-full lg:max-w-full lg:flex">
+                <img src={image} className=" w-full h-48 lg:h-auto lg:w-60 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" />
+                <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8">
+                        <p className="text-sm text-gray-600 flex items-center">
+                            {date}
+                        </p>
+                        <div className="text-gray-900 font-bold text-xl mb-2">{title}</div>
+                        <p className="mt-4 text-gray-400 font-semibold">{category}</p>
+                        <p className="text-gray-700 text-base"> {description}</p>
+                    </div>
+                    <div className="flex items-center">
+                        <Link to={`/blog/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )   
+}
+
+export default Card
